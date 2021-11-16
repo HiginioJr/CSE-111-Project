dbbae2465f16e7c2931df406380f68b79ff9cf74
Aramy Truong <aramytruong@gmail.com>
2021-11-15 22:24:58 -0800

    Added database file
    Added database.sqlite. Is subject to change.

--- tables.sql
+++ tables.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS parks;
 CREATE TABLE parks (
-    p_park_code string(100) not null,
+    p_park_code string(4) not null,
     p_park_name string(100) not null,
-    p_state string(100) not null,
-    p_acres string(100) not null,
-    p_latitude string(100) not null,
-    p_longitude string(100) not null
+    p_state string(2) not null,
+    p_acres int not null,
+    p_latitude real not null,
+    p_longitude real not null
 );
 
 DROP TABLE IF EXISTS species;
@@ -13,12 +13,12 @@ CREATE TABLE species (
     s_species_id string(100) not null,
     s_park_name string(100) not null,
     s_category string(100) not null,
-    s_order string(100) not null,
-    s_family string(100) not null,
-    s_occurrence string(100) not null,
-    s_nativeness string(100) not null,
-    s_abundance string(100) not null,
-    s_seasonality string(100) not null
+    s_order string(100),
+    s_family string(100),
+    s_occurrence string(100),
+    s_nativeness string(100),
+    s_abundance string(100),
+    s_seasonality string(100)
 );
 
 DROP TABLE IF EXISTS animals;
@@ -26,7 +26,7 @@ CREATE TABLE animals (
     a_species_id string(100) not null,
     a_scientific_name string(100) not null,
     a_record_status string(100) not null,
-    a_conservation_ststus string(100) not null
+    a_conservation_ststus string(100)
 );
 
 DROP TABLE IF EXISTS plants;
@@ -34,7 +34,7 @@ CREATE TABLE plants (
     pl_species_id string(100) not null,
     pl_scientific_name string(100) not null,
     pl_record_status string(100) not null,
-    pl_conservation_status string(100) not null
+    pl_conservation_status string(100)
 );
 
 DROP TABLE IF EXISTS common_names;
@@ -46,7 +46,7 @@ CREATE TABLE common_names (
 DROP TABLE IF EXISTS search;
 CREATE TABLE search (
     se_animal_name string(100) not null,
-    se_park string(100) not null
+    se_park string(100)
 );
 
 DROP TABLE IF EXISTS user;
